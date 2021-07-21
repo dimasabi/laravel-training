e21b96638eba8bd9369bda157f71e282aa36ac4e
dimasabi <87745486+dimasabi@users.noreply.github.com>
2021-07-21 15:32:48 +0700

    Add files via upload

---
+++ ex1ganjilgenap.php
@@ -0,0 +1,10 @@
+<?php
+
+$angka=10; // Inisialisasi variabel bil dengan nilai 10
+
+if ($angka % 2 == 0){ //Kondisi
+    echo "Genap"; //Kondisi true
+}else {
+    echo "Ganjil"; //Kondisi false
+}
+?>
---
+++ ex2konversi.php
@@ -0,0 +1,20 @@
+<?php
+
+$angka=65; // Inisialisasi variabel bil dengan nilai tertentu
+
+if ($angka == 100){ //Kondisi
+    echo "A"; 
+}
+else if ($angka >= 80){ //
+    echo "B"; 
+}
+else if ($angka >= 70){ //
+    echo "C"; 
+}
+else if ($angka >= 60){ //
+    echo "D"; 
+}
+else if ($angka <= 59){ //
+    echo "E"; 
+}
+?>
